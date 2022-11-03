ed15cc2c1a8d7faa5796b842b5c23f64218fd8d0
miajacobs21 <mjacobs4@live.maryville.edu>
2022-11-02 22:07:36 -0400

    655final Project startup

--- src/app/tab4/tab4.page.spec.ts
+++ src/app/tab4/tab4.page.spec.ts
@@ -1,21 +1,20 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-import { Component } from '@angular/core';
 
-import { Tab3Page } from './tab3.page';
+import { Tab4Page } from './tab4.page';
 
-describe('Tab3Page', () => {
-  let component: Tab3Page;
-  let fixture: ComponentFixture<Tab3Page>;
+describe('Tab4Page', () => {
+  let component: Tab4Page;
+  let fixture: ComponentFixture<Tab4Page>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab3Page],
+      declarations: [Tab4Page],
       imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Tab3Page);
+    fixture = TestBed.createComponent(Tab4Page);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
@@ -24,12 +23,3 @@ describe('Tab3Page', () => {
     expect(component).toBeTruthy();
   });
 });
-
-
-
-@Component({
-  selector: 'app-example',
-  templateUrl: 'example.component.html',
-  styleUrls: ['example.component.css'],
-})
-export class ExampleComponent {}
